c3d63d796375c4e0fcb68a0f5a5b7678d82614c4
Renato Silva <dev.renatosilva@gmail.com>
2021-12-02 00:47:39 -0300

    init config

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sport Time
+<div align="center">
+  <h1>🏀⚽🏈 <br> Uma aplicação Vue que exibe todos os horários, resultados e live score de partidas esportivas.</h1>
+</div>
+
---
+++ src/views/home/Index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="">
+        <div class="home">
+            <small class="display-3"> 🏀🏈⚽ </small>
+            <p class="display-1 text-primary"> SPORTIME </p>  
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+    mounted() {
+        document.querySelector('body').setAttribute('style', 'background:#000')
+    },
+}
+</script>
+
+<style scoped>
+.home{
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  font-family: 'Oswald', sans-serif;
+}
+</style>
